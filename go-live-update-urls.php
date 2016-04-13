e626add3da281a0fab2c7ac4bb98c13e30d56496
lipemat <mat@matlipe.com>
2016-04-13 17:04:18 -0400

    bump to version 3.1.2

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -5,10 +5,10 @@ Plugin URI: https://matlipe.com/go-live-update-urls/
 Description: Updates all the URLs in the database to point to the new URL when making your site live or changing domains.
 Author: Mat Lipe
 Author URI: https://matlipe.com/
-Version: 3.1.0
+Version: 3.1.2
 Text Domain: go-live-update-urls
 */
-define( 'GLUU_VERSION', "3.1.0" );
+define( 'GLUU_VERSION', "3.1.2" );
 
 define( 'GLUU_VIEWS_DIR', plugin_dir_path(__FILE__) . 'views/' );
 define( 'GLUU_URL_VIEWS_DIR', plugins_url('go-live-update-urls').'/views/' );
