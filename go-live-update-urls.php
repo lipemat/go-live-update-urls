18d3035aa611959d566c1c1cdc0da21ccc88dde2
lipemat <mat@matlipe.com>
2018-05-17 09:21:55 -0400

    support updating url encoded urls

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -34,16 +34,15 @@ function go_live_update_urls_load() {
 function go_live_update_urls_autoload( $class ) {
 	$classes = array(
 		//core
-		'Go_Live_Update_Urls_PHP_5_2_Mock_Class'  => 'PHP_5_2_Mock_Class.php',
-		'Go_Live_Update_Urls_Admin_Page'          => 'Admin_Page.php',
-		'Go_Live_Update_Urls_Core'                => 'Core.php',
-		'Go_Live_Update_Urls_Database'            => 'Database.php',
+		'Go_Live_Update_Urls_PHP_5_2_Mock_Class'     => 'PHP_5_2_Mock_Class.php',
+		'Go_Live_Update_Urls_Admin_Page'             => 'Admin_Page.php',
+		'Go_Live_Update_Urls_Core'                   => 'Core.php',
+		'Go_Live_Update_Urls_Database'               => 'Database.php',
 		//updaters
-		'Go_Live_Update_Urls__Updaters__Abstract' => 'Updaters/Abstract.php',
-		'Go_Live_Update_Urls__Updaters__JSON'     => 'Updaters/JSON.php',
-		'Go_Live_Update_Urls__Updaters__Repo'     => 'Updaters/Repo.php',
-
-
+		'Go_Live_Update_Urls__Updaters__Abstract'    => 'Updaters/Abstract.php',
+		'Go_Live_Update_Urls__Updaters__JSON'        => 'Updaters/JSON.php',
+		'Go_Live_Update_Urls__Updaters__Repo'        => 'Updaters/Repo.php',
+		'Go_Live_Update_Urls__Updaters__Url_Encoded' => 'Updaters/Url_Encoded.php',
 	);
 	if ( isset( $classes[ $class ] ) ) {
 		require dirname( __FILE__ ) . '/src/' . $classes[ $class ];
