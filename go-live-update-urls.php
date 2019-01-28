4fb30a20efaf0f5fee1801cb066a399e4a9eaa93
lipemat <mat@matlipe.com>
2019-01-28 10:06:06 -0500

    bump to 5.2.4

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -5,13 +5,13 @@
  * Description: Updates all the URLs in the database to point to a new URL when making your site live or changing domains.
  * Author: Mat Lipe
  * Author URI: https://matlipe.com/
- * Version: 5.2.3
+ * Version: 5.2.4
  * Text Domain: go-live-update-urls
  *
  * @package go-live-update-urls
  */
 
-define( 'GO_LIVE_UPDATE_URLS_VERSION', '5.2.3' );
+define( 'GO_LIVE_UPDATE_URLS_VERSION', '5.2.4' );
 
 /**
  * Load the plugin
