8e470fef1c4ba3885c621e865b3966c0bf27f06b
lipemat <mat@matlipe.com>
2019-01-28 10:05:08 -0500

    Fix issue when database column is named the same as a sQL command (e.g. wpml)

--- src/Database.php
+++ src/Database.php
@@ -5,14 +5,13 @@
  *
  * @author Mat Lipe
  * @since  2/1/2018
- *
  */
 class Go_Live_Update_Urls_Database {
 	protected $old_url = false;
 
 	protected $new_url = false;
 
-	public $double_subdomain = false; //keep track if going to a subdomain
+	public $double_subdomain = false; // keep track if going to a subdomain
 
 
 	/**
@@ -24,7 +23,7 @@ class Go_Live_Update_Urls_Database {
 	 */
 	public function get_serialized_tables() {
 		global $wpdb;
-		//default tables with serialized data
+		// default tables with serialized data
 		$serialized_tables = array(
 			$wpdb->options     => 'option_value',
 			$wpdb->postmeta    => 'meta_value',
@@ -33,15 +32,14 @@ class Go_Live_Update_Urls_Database {
 			$wpdb->usermeta    => 'meta_value',
 		);
 
-		//we are not going to update user meta if we are not on main blog
+		// we are not going to update user meta if we are not on main blog
 		if ( is_multisite() ) {
 			$serialized_tables[ $wpdb->sitemeta ] = 'meta_value';
-			//WP 5.0.0+
+			// WP 5.0.0+
 			if ( isset( $wpdb->blogmeta ) ) {
 				$serialized_tables[ $wpdb->blogmeta ] = 'meta_value';
 			}
 		}
-
 
 		$tables = apply_filters( 'go-live-update-urls-serialized-tables', $serialized_tables );
 
@@ -99,7 +97,7 @@ class Go_Live_Update_Urls_Database {
 			$tables[] = $wpdb->sitecategories;
 			$tables[] = $wpdb->registration_log;
 			$tables[] = $wpdb->blog_versions;
-			//WP 5.0.0+
+			// WP 5.0.0+
 			if ( isset( $wpdb->blogmeta ) ) {
 				$tables[ $wpdb->blogmeta ];
 			}
@@ -121,15 +119,15 @@ class Go_Live_Update_Urls_Database {
 		global $wpdb;
 		$query = "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA='" . $wpdb->dbname . "' AND TABLE_NAME LIKE '" . $wpdb->prefix . "%'";
 
-		//Site 1's 'LIKE wp_%' will return all tables in the database
-		//so we exclude all possible sub sites e.g. wp_2, wp_3 up to 9
+		// Site 1's 'LIKE wp_%' will return all tables in the database
+		// so we exclude all possible sub sites e.g. wp_2, wp_3 up to 9
 		$not_like = null;
 		if ( 1 === (int) $wpdb->blogid && is_multisite() ) {
 			for ( $i = 1; $i <= 9; $i ++ ) {
 				$not_like .= "'" . $wpdb->prefix . $i . "',";
 			}
 			$not_like = substr( $not_like, 0, - 1 );
-			$query    .= ' AND SUBSTRING(TABLE_NAME,1,4) NOT IN (' . $not_like . ')';
+			$query   .= ' AND SUBSTRING(TABLE_NAME,1,4) NOT IN (' . $not_like . ')';
 		}
 		return $wpdb->get_col( $query );
 	}
@@ -158,9 +156,9 @@ class Go_Live_Update_Urls_Database {
 	 *
 	 * @since 5.0.0
 	 *
-	 * @param string $old_url
-	 * @param string $new_url
-	 * @param array  $tables
+	 * @param string $old_url - the old URL.
+	 * @param string $new_url - the new URL.
+	 * @param array  $tables - the tables we are going to update.
 	 *
 	 * @todo  split this functionality into its own OOP class
 	 *
@@ -175,15 +173,15 @@ class Go_Live_Update_Urls_Database {
 
 		$updaters = (array) Go_Live_Update_Urls__Updaters__Repo::instance()->get_updaters();
 
-		// If the new domain is the old one with a new sub-domain like www
+		// If the new domain is the old one with a new sub-domain like www.
 		if ( strpos( $this->new_url, $this->old_url ) !== false ) {
-			list( $subdomain ) = explode( '.', $this->new_url );
+			list( $subdomain )      = explode( '.', $this->new_url );
 			$this->double_subdomain = $subdomain . '.' . $this->new_url;
 		}
 
-		$tables            = apply_filters( 'go-live-update-urls/database/update-tables', $tables, $this );
+		$tables = apply_filters( 'go-live-update-urls/database/update-tables', $tables, $this );
 
-		// Backward compatibility
+		// Backward compatibility.
 		if ( array_values( $tables ) !== $tables ) {
 			$tables = (array) array_flip( $tables );
 		}
@@ -195,41 +193,49 @@ class Go_Live_Update_Urls_Database {
 			$serialized->update_all_serialized_tables();
 		}
 
-		//Go through each table sent to be updated
+		// Go through each table sent to be updated.
 		foreach ( (array) $tables as $table ) {
 			$column_query = "SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA='{$wpdb->dbname}' AND TABLE_NAME=%s";
 			$columns      = $wpdb->get_col( $wpdb->prepare( $column_query, $table ) );
 
 			foreach ( $columns as $_column ) {
-				$update_query = 'UPDATE ' . $table . ' SET ' . $_column . ' = replace(' . $_column . ', %s, %s)';
+				$update_query = 'UPDATE ' . $table . ' SET `' . $_column . '` = replace(`' . $_column . '`, %s, %s)';
 				$wpdb->query( $wpdb->prepare( $update_query, array( $this->old_url, $this->new_url ) ) );
 
-				//Run each updater
+				// Run each updater.
 				foreach ( $updaters as $_updater_class ) {
 					if ( class_exists( $_updater_class ) ) {
 						/** @var Go_Live_Update_Urls__Updaters__Abstract $_updater */
 						$_updater = new $_updater_class( $table, $_column, $this->old_url, $this->new_url );
 						$_updater->update_data();
-						//run each updater through double sub-domain if applicable
-						if ( $this->double_subdomain ) {
+						if ( ! empty( $this->double_subdomain ) ) {
 							$_updater = new $_updater_class( $table, $_column, $this->double_subdomain, $this->new_url );
 							$_updater->update_data();
 						}
 					}
 				}
 
-
-				//Fix the dub dubs if this was the old domain with a new sub
+				// Fix the dub dubs if this was the old domain with a new sub
 				if ( $this->double_subdomain ) {
-					$wpdb->query( $wpdb->prepare( $update_query, array(
-						$this->double_subdomain,
-						$this->new_url,
-					) ) );
-					//Fix the emails breaking by being appended the new subdomain
-					$wpdb->query( $wpdb->prepare( $update_query, array(
-						'@' . $this->new_url,
-						'@' . $this->old_url,
-					) ) );
+					$wpdb->query(
+						$wpdb->prepare(
+							$update_query,
+							array(
+								$this->double_subdomain,
+								$this->new_url,
+							)
+						)
+					);
+					// Fix the emails breaking by being appended the new subdomain
+					$wpdb->query(
+						$wpdb->prepare(
+							$update_query,
+							array(
+								'@' . $this->new_url,
+								'@' . $this->old_url,
+							)
+						)
+					);
 				}
 			}
 		}
@@ -246,7 +252,7 @@ class Go_Live_Update_Urls_Database {
 
 	}
 
-	//********** SINGLETON **********/
+	// ********** SINGLETON **********/
 
 
 	/**
