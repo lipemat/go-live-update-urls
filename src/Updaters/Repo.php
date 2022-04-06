3442bc5bbd38770c56905b42d9ea324783e91e1a
lipemat <mat@matlipe.com>
2022-04-06 15:21:46 -0400

    Fix PHPDocs for Repo so PHPStan will pass

--- src/Updaters/Repo.php
+++ src/Updaters/Repo.php
@@ -14,10 +14,9 @@ class Repo {
 	use Singleton;
 
 	/**
-	 * Get all registered updaters by classname
-	 * This list will grow over time as things are converted over
+	 * Get all registered updaters by classname.
 	 *
-	 * @return array<Updaters_Abstract::class>
+	 * @return string[]
 	 */
 	public function get_updaters() {
 		$updaters = apply_filters( 'go-live-update-urls/updaters/repo/updaters', [
