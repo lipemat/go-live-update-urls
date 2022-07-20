18a69a490c615aff1925fe473a6eb4e5c373fae0
lipemat <mat@matlipe.com>
2022-07-20 11:50:45 -0400

    Require PRO version 6.8.0

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -17,7 +17,7 @@
  */
 
 define( 'GO_LIVE_UPDATE_URLS_VERSION', '6.3.9' );
-define( 'GO_LIVE_UPDATE_URLS_REQUIRED_PRO_VERSION', '6.6.0' );
+define( 'GO_LIVE_UPDATE_URLS_REQUIRED_PRO_VERSION', '6.8.0' );
 define( 'GO_LIVE_UPDATE_URLS_URL', plugin_dir_url( __FILE__ ) );
 
 use Go_Live_Update_Urls\Admin;
