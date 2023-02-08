e74acd4a7570d827925568d04b1b0b898c35be0b
lipemat <mat@matlipe.com>
2023-02-08 18:08:41 -0500

    Modernize the go-live-update-urls.js file

--- resources/go-live-update-urls.js
+++ resources/go-live-update-urls.js
@@ -1,22 +1,16 @@
-(function( $ ){
-	var go_live_update_urls = {
-		init : function(){
-			$( '[data-js="go-live-update-urls/checkboxes/check-all"]' ).click( this._un_check_tables );
-		},
+/**
+ * Check-all functionality on tools page.
+ *
+ * @package go-live-update-urls
+ */
 
-		_un_check_tables : function(){
-			var el = $( this );
-			if( el.prop( 'checked' ) ){
-				$( '[data-list="' + el.data( 'list' ) +'"] input' ).prop( 'checked', true );
-			} else {
-				$( '[data-list="' + el.data( 'list' ) +'"] input' ).prop( 'checked', false );
-			}
+jQuery( function ( $ ) {
+	$( '[data-js="go-live-update-urls/checkboxes/check-all"]' ).on( 'click', function () {
+		var el = $( this );
+		if ( el.prop( 'checked' ) ) {
+			$( '[data-list="' + el.data( 'list' ) + '"] input' ).prop( 'checked', true );
+		} else {
+			$( '[data-list="' + el.data( 'list' ) + '"] input' ).prop( 'checked', false );
 		}
-	};
-
-	$( function(){
-		go_live_update_urls.init();
-	} );
-
-
-})( jQuery );
+	} )
+} )
