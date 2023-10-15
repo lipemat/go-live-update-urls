5dceb4bcc9909504582cb0a9a739adcf9d232d0f
lipemat <mat@matlipe.com>
2023-10-14 21:45:36 -0500

    Use level 6 for phpstan with the baseline intact

--- dev/phpstan-baseline.php
+++ dev/phpstan-baseline.php
@@ -7,7 +7,92 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/../src/Admin.php',
 ];
 $ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Admin\\:\\:admin_page\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Admin.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Admin\\:\\:hook\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Admin.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Admin\\:\\:register_admin_page\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Admin.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Admin\\:\\:render_check_boxes\\(\\) has parameter \\$tables with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Admin.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Admin\\:\\:success\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Admin.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:flush_caches\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Core.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:hook\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Core.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:plugin_action_link\\(\\) has parameter \\$actions with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Core.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:plugin_action_link\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Core.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:count_database_urls\\(\\) has parameter \\$tables with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_all_table_names\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_column_types\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_core_tables\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_custom_plugin_tables\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_serialized_tables\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:hook\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:update_the_database\\(\\) has parameter \\$tables with no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Database.php',
 ];
@@ -17,9 +102,54 @@ $ignoreErrors[] = [
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:has_missing_classes\\(\\) has parameter \\$data with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:replace_tree\\(\\) has parameter \\$data with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:replace_tree\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:set_dry_run\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:update_all_serialized_tables\\(\\) has parameter \\$tables with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
 	'count' => 2,
 	'path' => __DIR__ . '/../src/Skip_Rows.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Skip_Rows\\:\\:hook\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Skip_Rows.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Go_Live_Update_Urls\\\\Skip_Rows\\:\\:\\$primary_keys type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Skip_Rows.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Go_Live_Update_Urls\\\\Skip_Rows\\:\\:\\$skip type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Skip_Rows.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Go_Live_Update_Urls\\\\Updaters\\\\Repo\\:\\:hook\\(\\) has no return type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Updaters/Repo.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
