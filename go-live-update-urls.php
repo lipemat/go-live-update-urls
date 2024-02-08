e7613ee59df141ac6b3d73376ca6f9435b51ac07
lipemat <mat@matlipe.com>
2024-02-08 16:32:44 -0600

    Bump required WP version to 6.1.0

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -9,7 +9,7 @@
  * Text Domain: go-live-update-urls
  * Domain Path: /languages/
  * Network: false
- * Requires at least: 6.0.0
+ * Requires at least: 6.1.0
  * Requires PHP: 7.2.0
  *
  * @package go-live-update-urls
