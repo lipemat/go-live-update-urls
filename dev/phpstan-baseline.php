0e9fe99476fbd56612c80a1af7c66ca558369a65
lipemat <mat@matlipe.com>
2024-07-11 19:44:36 -0400

    Update phpstan baseline for latest rules

--- dev/phpstan-baseline.php
+++ dev/phpstan-baseline.php
@@ -2,81 +2,319 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
+	// identifier: missingType.return
 	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:flush_caches\\(\\) has no return type specified\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Core.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:plugin_action_link\\(\\) has parameter \\$actions with no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Core.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Core\\:\\:plugin_action_link\\(\\) return type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Core.php',
 ];
 $ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `blogid` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `blogmeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `blogs` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `commentmeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `comments` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `dbname` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `links` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `options` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `postmeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `posts` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `prefix` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `registration_log` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `signups` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `site` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `sitecategories` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `sitemeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `term_relationships` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `term_taxonomy` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `termmeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `terms` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `usermeta` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `users` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `esc_like` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `get_col` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `get_var` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `prepare` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 3,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `query` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Database.php',
+];
+$ignoreErrors[] = [
+	// identifier: empty.notAllowed
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Database.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_all_table_names\\(\\) return type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Database.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:get_serialized_tables\\(\\) return type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Database.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Database\\:\\:update_the_database\\(\\) has parameter \\$tables with no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Database.php',
 ];
 $ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `Column_name` property on unknown `\\$pk\\[0\\]` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `column` property on unknown `\\$row` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `primary_key_column` property on unknown `\\$row` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `get_results` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 3,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `prepare` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `query` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Serialized.php',
+];
+$ignoreErrors[] = [
+	// identifier: empty.notAllowed
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
 	'count' => 3,
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:has_missing_classes\\(\\) has parameter \\$data with no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:replace_tree\\(\\) has parameter \\$data with no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:replace_tree\\(\\) return type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.return
 	'message' => '#^Method Go_Live_Update_Urls\\\\Serialized\\:\\:set_dry_run\\(\\) has no return type specified\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Serialized.php',
 ];
 $ignoreErrors[] = [
+	// identifier: empty.notAllowed
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
 	'count' => 2,
 	'path' => __DIR__ . '/../src/Skip_Rows.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Property Go_Live_Update_Urls\\\\Skip_Rows\\:\\:\\$primary_keys type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Skip_Rows.php',
 ];
 $ignoreErrors[] = [
+	// identifier: missingType.iterableValue
 	'message' => '#^Property Go_Live_Update_Urls\\\\Skip_Rows\\:\\:\\$skip type has no value type specified in iterable type array\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Skip_Rows.php',
 ];
 $ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `dbname` property on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Updates.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownProperty
+	'message' => '#^Accessing `type` property on unknown `\\$column` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Updates.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `get_results` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Updates.php',
+];
+$ignoreErrors[] = [
+	// identifier: lipemat.noUnknownMethodCaller
+	'message' => '#^Calling `prepare` method on unknown `\\$wpdb` can skip important errors\\. Make sure the type is known\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/../src/Updates.php',
+];
+$ignoreErrors[] = [
+	// identifier: empty.notAllowed
 	'message' => '#^Construct empty\\(\\) is not allowed\\. Use more strict comparison\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../src/Updates.php',
